59024de80c309542d181b6f73df4d42bf4a99484
tuck <sampsont2@wit.edu>
2026-07-13 12:06:04 -0400

    Add capitalize function for string case conversion
    Implement capitalize function to convert lowercase letters to uppercase and handle NULL input.

--- capitalize.c
+++ capitalize.c
@@ -1,10 +1,38 @@
 #include <stddef.h> // For NULL
-// TODO: Include any necessary header files here
+#include <stdlib.h> // For malloc
+#include <ctype.h>  // For toupper
 
 /**
- * TODO: Describe what the function does
+ * Returns a newly allocated string with all lowercase letters
+ * converted to uppercase.
  */
-char* capitalize(const char* s) {
-    return NULL;
-}
+char* capitalize(const char* s)
+{
+    if (s == NULL)
+    {
+        return NULL;
+    }
 
+    int length = 0;
+
+    while (s[length] != '\0')
+    {
+        length++;
+    }
+
+    char *result = malloc(sizeof(char) * (length + 1));
+
+    if (result == NULL)
+    {
+        return NULL;
+    }
+
+    for (int i = 0; i < length; i++)
+    {
+        result[i] = toupper((unsigned char)s[i]);
+    }
+
+    result[length] = '\0';
+
+    return result;
+}
