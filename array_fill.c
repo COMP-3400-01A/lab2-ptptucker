b6557da37cd227097b8465cfbc47c16e67374027
tuck <sampsont2@wit.edu>
2026-07-13 11:57:04 -0400

    Added array_fill function to populate an array
    Implement array_fill function to fill an array with a range of integers.

--- array_fill.c
+++ array_fill.c
@@ -4,5 +4,14 @@
  * TODO: Describe what the function does
  */
 int array_fill(int* array, int array_len, int begin, int end) {
-    return 1;
+    int count = 0;
+
+    for (int i = begin; i <= end && count < array_len; i++)
+    {
+        array[count] = i;
+        count++;
+    }
+
+    return count;
+}
 }
