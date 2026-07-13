18043272b07182dd7b8e8ff1f1f5d29826ec9267
tuck <sampsont2@wit.edu>
2026-07-13 12:04:53 -0400

    Implemented array_create_evens function
    Implements a function to create an array of even numbers.

--- array_create.c
+++ array_create.c
@@ -1,9 +1,50 @@
 #include <stddef.h> // For NULL
-// TODO: Include any necessary header files here
+#include <stdlib.h> // For malloc
 
 /**
- * TODO: Describe what the function does
+ * Creates a dynamically allocated array containing all even numbers
+ * from begin to end (inclusive). Returns NULL if no such range exists.
  */
-int* array_create_evens(int begin, int end) {
-    return NULL;
+int* array_create_evens(int begin, int end)
+{
+    if (begin >= end)
+    {
+        return NULL;
+    }
+
+    // Find the first even number
+    int first;
+    if (begin % 2 == 0)
+    {
+        first = begin;
+    }
+    else
+    {
+        first = begin + 1;
+    }
+
+    if (first > end)
+    {
+        return NULL;
+    }
+
+    // Number of even numbers in the range
+    int length = ((end - first) / 2) + 1;
+
+    int *array = malloc(sizeof(int) * length);
+
+    if (array == NULL)
+    {
+        return NULL;
+    }
+
+    // Fill the array with even numbers
+    int value = first;
+    for (int i = 0; i < length; i++)
+    {
+        array[i] = value;
+        value += 2;
+    }
+
+    return array;
 }
