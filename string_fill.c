1b13f4d5c2138f4040c8016ff9797bae3abe6515
tuck <sampsont2@wit.edu>
2026-07-13 12:01:31 -0400

    Implement string_fill function
    Implement string_fill function to fill an array with a character and null terminate.

--- string_fill.c
+++ string_fill.c
@@ -1,7 +1,25 @@
 #include <stddef.h> // For NULL
 
 /**
- * TODO: Describe what the function does
+ * Fills the destination array with the given character and
+ * ends the string with a null terminator.
  */
-void string_fill(char dest[], int dest_cap, char c) {
+void string_fill(char dest[], int dest_cap, char c)
+{
+    if (dest == NULL)
+    {
+        return;
+    }
+
+    if (dest_cap <= 0)
+    {
+        return;
+    }
+
+    for (int i = 0; i < dest_cap - 1; i++)
+    {
+        dest[i] = c;
+    }
+
+    dest[dest_cap - 1] = '\0';
 }
